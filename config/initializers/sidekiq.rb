3ca7921e3490b6fa716b961a6b3aa753b269d2a5
John Romkey <58883+romkey@users.noreply.github.com>
2025-12-09 20:45:33 -0800

    Fix Sidekiq queue names in production to match ActiveJob prefix
    - Production uses queue_name_prefix 'member_manager_production'
    - Jobs were being enqueued to prefixed queues but Sidekiq was listening to unprefixed queues
    - Update Sidekiq config to use prefixed queue names in production
    - Fixes issue where jobs were queued but not processed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,21 @@ redis_url = ENV.fetch('REDIS_URL', 'redis://redis:6379/0')
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
+  
   # Configure Sidekiq to listen to queues with the environment-specific prefix
-  config.queues = [
-    "default",
-    "mailers"
-  ]
+  # In production, ActiveJob uses queue_name_prefix, so we need to match it
+  if Rails.env.production?
+    queue_prefix = Rails.application.config.active_job.queue_name_prefix
+    config.queues = [
+      "#{queue_prefix}_default",
+      "#{queue_prefix}_mailers"
+    ]
+  else
+    config.queues = [
+      "default",
+      "mailers"
+    ]
+  end
 end
 
 Sidekiq.configure_client do |config|
